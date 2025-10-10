a90d55c48a8ccc0a1b065bdf4e1c219e3cf76d0b
cmesyarif <gitsyarif@gmail.com>
2025-10-10 23:50:54 +0800

    Add a function for a single round play

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 // Get the human choice
 function getHumanChoice() {
@@ -19,4 +19,30 @@ function getHumanChoice() {
     return choice.toLowerCase(); // make the input case-insensitive
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+// Creating score variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Play a single round
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie! Both chose " + humanChoice);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
